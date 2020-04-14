9e356bb94ab25923cccbae1817ee31d4ae7832c0
venomek <venom4ek@gmail.com>
2020-04-14 21:17:47 +0300

    fixed #1

--- bonus-service/src/main/java/Main.java
+++ bonus-service/src/main/java/Main.java
@@ -5,7 +5,10 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-    service.calculate(amount, registered);
+    long actual = service.calculate(amount, registered);
+
+    System.out.println(actual);
+
   }
 }
 
